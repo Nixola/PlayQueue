35d24fc7f7e6e44615aec75dcdb0fe324adb324c
Nix <niki.or97@yahoo.it>
2016-10-17 15:18:51 +0200

    Note amplitude is used

--- thread.lua
+++ thread.lua
@@ -145,7 +145,7 @@ while true do
         local ratio = 2^(sin(tau * 6 * note.ttime)/ 6 / 12 )
         note.phase = note.phase + t * ratio -- f2 / f1
         --io.write(f1, "\t", f2, "\n")
-        sample = sample + note.func(note.phase, f1) * a --instrs[note.instrument](note.phase, f1) * a
+        sample = sample + note.func(note.phase, f1) * a * note.amplitude--instrs[note.instrument](note.phase, f1) * a
         --print(sample)
 
         note.time = time

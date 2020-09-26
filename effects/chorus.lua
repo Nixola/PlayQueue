18ef311600672ed8f3aef84bfbd0d217f9c56c5d
Nicola Orlando <nicolaorlando24@gmail.com>
2020-09-26 23:30:15 +0200

    Fixed chorus, edited flanger

--- effects/chorus.lua
+++ effects/chorus.lua
@@ -2,9 +2,8 @@ return function(effects, waveforms)
   return {
     init = function(self, note, shift)
       shift = shift or 0.1
-      local n1 = effects.flanger.init(note, shift)[1]
-      note.amplitude = note.amplitude * 2
-      local n2 = effects.flanger.init(note, -shift)[1]
+      local n1 = effects.flanger(effects, waveforms):init(note, shift)[1]
+      local n2 = effects.flanger(effects, waveforms):init(note, -shift)[1]
       return {n1, n2}
     end
   }
--- effects/flanger.lua
+++ effects/flanger.lua
@@ -3,8 +3,8 @@ return function(effects, waveforms)
     init = function(self, note, shift)
       shift = shift or 0.1
       local t = {}
-      note.amplitude = note.amplitude / 2
       t[1] = table.clone(note)
+      t[1].amplitude = note.amplitude / 5
       t[1].frequency = t[1].frequency + shift
       return t
     end

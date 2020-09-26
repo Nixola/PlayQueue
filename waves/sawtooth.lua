b3f936268c7abf27623c9e8edab664454d89ab6e
Nicola Orlando <nicolaorlando24@gmail.com>
2020-09-26 23:15:46 +0200

    Added square and saw waves by Zorg. Sound much better than mine.

--- waves/sawtooth.lua
+++ waves/sawtooth.lua
@@ -1,1 +1,11 @@
-return function(x, frequency) return x * frequency * 2 % 2 - 1 end
+return function(phase, frequency)
+    local result = 0
+    local x = 0
+    local n = math.ceil(SR / 2 / frequency)
+    for i=1, n do
+        x = math.sin(phase * i * frequency * 2 * math.pi)
+        x = x / i -- amplitude dropoff
+        result = result + x
+    end
+    return result
+end

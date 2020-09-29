c1f8317e274844e7cd1ec5f3d4a406e23300ea77
Nicola Orlando <nicolaorlando24@gmail.com>
2020-09-29 17:27:41 +0200

    Added test 'strings' instrument

--- instruments.lua
+++ instruments.lua
@@ -1,13 +1,3 @@
-local sq = {{amplitude = 0.5, keyshift = 0, waveform = "sine"}}
-for i = 1, 31, 2 do
-  sq[#sq+1] = {amplitude = 0.5/i, keyshift = math.log(i) / math.log(2^(1/12)), waveform = "sine"}
-end
-
-local saw = {{amplitude = 0.5, keyshift = 0, waveform = "sine"}}
-for i = 1, 20 do
-  saw[#saw+1] = {amplitude = (0.5/i) * (-1)^(i+1), keyshift = math.log(i) / math.log(2^(1/12)), waveform = "sine"}
-end
-
 return {
   sine = {
     {amplitude = 1, keyshift = 0, waveform = "sine", effects = {{type = "vibrato", 6, 1/6}} }
@@ -25,7 +15,7 @@ return {
     {amplitude = 0.4, keyshift = 24,  waveform = "sine", effects = {{type = "vibrato", 3, 1/10}} },
     --{amplitude = 0.3, keyshift = 36,  waveform = "sine", effects = {{type = "vibrato", 3, 1/10}} }
   },
-  ---[[
+
   saw = {
     {amplitude = .6, keyshift = 0, waveform = "sawtooth", effects = {{type = "chorus", .1}}},
   },
@@ -33,11 +23,6 @@ return {
   square = {
     {amplitude = .6, keyshift = 0, waveform = "square", effects = {{type = "chorus", .1}}},
   },
-  --[=[
-  --]]
-  saw = saw,
-  square = sq,
-  --]=]
 
   double = {
     {amplitude = 1, keyshift = 0, waveform = "sine", effects = {{type = "vibrato", 6, 1/6}} },
@@ -56,8 +41,9 @@ return {
     {amplitude = 1, keyshift = 0, waveform = "expow"},
   },
 
-  cantor = {
-    {amplitude = 1, keyshift = 0, waveform = "cantor"}
+  strings = {
+  	{amplitude = 0.8, keyshift = 0, waveform = "minkQM"},
+  	{amplitude = 0.8, keyshift = 0, waveform = "expow"},
   },
 
   vibraphone = {

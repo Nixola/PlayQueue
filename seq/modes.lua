565024dc0c99101862ea4cc6f69cf61e92d6de78
Nicola Orlando <nicolaorlando24@gmail.com>
2023-07-07 16:04:30 +0200

    Added default normal mode

--- seq/modes.lua
+++ seq/modes.lua
@@ -1,7 +1,22 @@
 local modes = {}
 
+
 modes.init = function(self, ...)
 	self.modes = {}
+	self.modes.Normal = {
+		name = "",
+		keys = {key = "escape"},
+		keypressed = function(_, k, kk, isRepeat)
+			local ctrl, shift, alt = love.keyboard.isDown("lctrl", "rctrl"), love.keyboard.isDown("lshift", "rshift"), love.keyboard.isDown("lalt", "ralt")
+			for i, v in pairs(self.modes) do
+				print(i, k == v.keys.key, ctrl == v.keys.ctrl , shift == v.keys.shift , alt == v.keys.alt, ctrl, v.keys.ctrl)
+				if k == v.keys.key and ctrl == v.keys.ctrl and shift == v.keys.shift and alt == v.keys.alt then
+					print("A")
+					self:switch(i)
+				end
+			end
+		end
+	}
 	local dir = love.filesystem.getDirectoryItems("seq/modes/")
 	for i, file in ipairs(dir) do
 		local f = love.filesystem.load("seq/modes/" .. file)
@@ -10,11 +25,20 @@ modes.init = function(self, ...)
 			error(mode)
 		end
 		self.modes[mode.name] = mode
-		mode = mode.insert --change to mode.normal
+		self.mode = self.modes.Insert --change to mode.normal
 		-- insert inserts note, either by click-dragging or by pressing keys
 		-- escape reverts to normal, which lets you select notes; selected notes can be deleted, dragged around, copied, pasted
 		-- t enters tempo mode, to change tempo; remove that ugly box in the top-left
 	end
+end
+
+modes.switch = function(self, mode)
+	if not self.modes[mode] then error(("Trying to set mode '%s' (doesn't exist)"):format(mode)) end
+	mode = self.modes[mode]
+	if mode.start then
+		mode:start()
+	end
+	self.mode = mode
 end
 
 
@@ -36,6 +60,9 @@ end
 
 
 modes.keypressed = function(self, k, kk, isRepeat)
+	if k == "escape" then
+		self:switch("Normal")
+	end
 	if self.mode.keypressed then
 		self.mode:keypressed(k, kk, isRepeat)
 	end
